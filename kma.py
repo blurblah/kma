4148ed3555948ce0dfef027a2e70a161b1a44c0e
blurblah <blurblah@blurblah.net>
2018-06-05 17:41:38 +0900

    Request current weather and create Current object for the response

--- kma.py
+++ kma.py
@@ -8,15 +8,25 @@ from current import Current
 from forecast import Forecast
 from datetime import datetime, timedelta
 import pytz
+import urllib3
+import json
+import certifi
+from urllib.parse import unquote
 
 
 class Weather(object):
     DATE_FMT = '%Y%m%d'
     TIME_FMT = '%H%M'
     FORECAST_TIME = ['0200', '0500', '0800', '1100', '1400', '1700', '2000', '2300']
+    ENDPOINT = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2'
 
     def __init__(self, api_key):
         self._api_key = api_key
+        self.http = urllib3.PoolManager(
+            cert_reqs='CERT_REQUIRED',
+            ca_certs=certifi.where()
+        )
+        self.SERVICE_KEY = 'sample_api_key'
 
     def _calculate_xy_point(self):
         ip_info = IPInfo()
@@ -50,8 +60,11 @@ class Weather(object):
             temp_time = (datetime.strptime(time, self.TIME_FMT)
                          - timedelta(hours=1)).strftime(self.TIME_FMT)
             time_param = temp_time[:2] + '00'
-
-        #return Current()
+        body = self._request_api('ForecastGrib', date_param, time_param)['response']['body']
+        return Current(self._find_value(body['items']['item'], 'T1H'),
+                       self._find_value(body['items']['item'], 'REH'),
+                       self._find_value(body['items']['item'], 'SKY'),
+                       self._find_value(body['items']['item'], 'RN1'))
 
     def get_forecast(self):
         date = self._get_date()
@@ -71,12 +84,33 @@ class Weather(object):
                           - timedelta(days=1)).strftime(self.DATE_FMT)
             time_index = len(self.FORECAST_TIME) - 1
         time_param = self.FORECAST_TIME[time_index]
+        print(self._request_api('ForecastSpaceData', date_param, time_param))
 
-        return Forecast()
+    def _request_api(self, api, base_date, base_time):
+        loc = self._calculate_xy_point()
+        # service key is encoded and urllib3 request will be encode parameters again
+        r = self.http.request('GET', self.ENDPOINT + '/' + api,
+                              fields={
+                                  'ServiceKey': unquote(self.SERVICE_KEY),
+                                  'base_date': base_date,
+                                  'base_time': base_time,
+                                  'nx': loc['x'],
+                                  'ny': loc['y'],
+                                  '_type': 'json'
+                              })
+        return json.loads(r.data.decode('utf-8'))
+
+    def _find_value(self, items, category):
+        #return [item for item in items if item['category'] == category]
+        for item in items:
+            if item['category'] == category:
+                return item['obsrValue']
+        return None
 
 
 if __name__ == '__main__':
     w = Weather('api_key')
-    w.get_forecast()
+    curr = w.get_current()
+    print(curr.temperature, curr.humidity, curr.sky, curr.rain_drop)
     #print(w.get_date())
     #print(w.get_time())
